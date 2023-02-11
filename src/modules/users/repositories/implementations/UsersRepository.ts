92d5a8d9633b5b0d40ef1d2fd7d0a378836e6e13
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-02-11 16:50:17 -0300

    :hammer: feat: add raw query and createQueryBuilder to a users module

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,8 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from "typeorm";
 
-import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
-import { User } from '../../entities/User';
-import { IUsersRepository } from '../IUsersRepository';
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
 
 export class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
@@ -15,16 +15,30 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+
+    const user = await this.repository
+      .createQueryBuilder("user")
+      .where("user.id = :id", {
+        id: user_id,
+      })
+      .getOne();
+
+    return user!;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY first_name ASC"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    // Select a user in users table where
+    // first_name = lower(first_name) and last_name = lower(last_name);
+
+    return this.repository.query(
+      `SELECT user FROM users WHERE first_name = LOWER(${first_name}) AND last_name = LOWER(${last_name})`
+    ); // Complete usando raw query
   }
 }
